6c13055baf2e60a3243b17f58d864f4b802b2a6c
Shane Holloway <shane.holloway@ieee.org>
2020-12-23 14:29:16 -0700

    Updated dependencies

--- test/rollup.config.js
+++ test/rollup.config.js
@@ -1,13 +1,12 @@
 import rpi_jsy from 'rollup-plugin-jsy-lite'
 import rpi_resolve from '@rollup/plugin-node-resolve'
 
-const sourcemap = 'inline'
 const plugins = [ rpi_jsy(), rpi_resolve() ]
 
 export default [
   { input: `./unittest.jsy`, plugins,
-    output: { file: './__unittest.cjs.js', format: 'cjs', sourcemap } },
+    output: { file: './__unittest.cjs.js', format: 'cjs', sourcemap: false } },
 
   { input: `./unittest.jsy`, context: 'window', plugins,
-    output: { file: './__unittest.iife.js', format: 'iife', name: `test_cbor_codec`, sourcemap } },
+    output: { file: './__unittest.iife.js', format: 'iife', name: `test_cbor_codec`, sourcemap: false } },
 ]
